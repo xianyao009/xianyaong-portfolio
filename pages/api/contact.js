93938d5566e7b0011d4c450ddf7019157a121761
xianyao009 <yao009@hotmail.com>
2022-07-31 23:05:31 -0700

    Fixed eslint warning

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,7 +2,7 @@ const mail = require("@sendgrid/mail");
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async (req, res) => {
+const handler =  async (req, res) => {
   const body = req.body;
 
   const message = `
@@ -24,3 +24,5 @@ export default async (req, res) => {
 
   res.status(200).json({ status: "OK" });
 };
+
+export default handler;
