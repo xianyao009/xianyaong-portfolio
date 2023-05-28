c1935290a2fb42dbd68f61b7833e40daed8fb135
xianyao009 <yao009@hotmail.com>
2023-05-27 23:55:03 -0700

    Update Projects page

--- components/container/ProjectsContainer.js
+++ components/container/ProjectsContainer.js
@@ -1,14 +1,72 @@
-import { Box, Center, Heading } from "@chakra-ui/react";
+import SocialMediaIcon from "../ui/SocialMediaIcon";
+import { FaGithub } from "react-icons/fa";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Center,
+  Divider,
+  Link,
+  Heading,
+  Stack,
+  SimpleGrid,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 const ProjectsContainer = () => {
+  const gridColumns = useBreakpointValue({ base: 1, md: 2 });
+
   return (
     <>
       <Center>
         <Heading>My Projects</Heading>
       </Center>
-      <Center>
-        <Box mt={3}>404 Projects Not Found</Box>
-      </Center>
+      <SimpleGrid columns={gridColumns}>
+        <Card maxW="250" m={10}>
+          <CardBody>
+            <Stack>
+              <Link href="https://xianyaong.com" isExternal>
+                <Heading>Portfolio Website</Heading>
+              </Link>
+              <Text>
+                A personal portfolio website showcasing my projects, skills, and
+                experiences, built with Next.js, Chakra UI, and Framer Motion.
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <SocialMediaIcon
+              href="https://github.com/xianyao009/xianyaong-portfolio"
+              label="github"
+              icon={FaGithub}
+              iconSize="1.25rem"
+            />
+          </CardFooter>
+        </Card>
+
+        <Card maxW="250" m={10}>
+          <CardBody>
+            <Stack>
+              <Heading>Taskdone To-do List</Heading>
+              <Text>
+                A simple and intuitive to-do list web application, built with
+                Next.js and Chakra UI.
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <SocialMediaIcon
+              href="https://github.com/xianyao009/taskdone"
+              label="github"
+              icon={FaGithub}
+              iconSize="1.25rem"
+            />
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
     </>
   );
 };
