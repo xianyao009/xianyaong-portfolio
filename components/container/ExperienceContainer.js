e6080af4bda33813ec762c8017931d3e598baa06
xianyao009 <yao009@hotmail.com>
2023-05-27 22:44:00 -0700

    Update Experience page

--- components/container/ExperienceContainer.js
+++ components/container/ExperienceContainer.js
@@ -23,7 +23,8 @@ const ExperienceContainer = () => {
       <Box mt={10}>
         <Tabs colorScheme="gray" isFitted isLazy>
           <TabList>
-            <Tab>Canalyst/Tegus</Tab>
+            <Tab>Tegus</Tab>
+            <Tab>Canalyst</Tab>
             <Tab>Noratek Solutions Inc</Tab>
           </TabList>
           <Box mt={3}>
@@ -39,7 +40,8 @@ const ExperienceContainer = () => {
                     }}
                   >
                     <Heading size="sm">Software Engineer Co-op</Heading>
-                    <Text fontSize={14}>May 2022 - December 2022</Text>
+                    <Text fontSize={14}>September 2022 - December 2022</Text>
+                    <Text fontSize={14}>May 2023 - August 2023</Text>
                     <List styleType="disc" spacing={2} mt={5}>
                       <ListItem>
                         Developed and maintained C# and Node.js application for
@@ -49,7 +51,7 @@ const ExperienceContainer = () => {
                       <ListItem>
                         Used HTML, CSS and Typescript to create an Excel Office
                         Add-in with custom functions for clients to pull
-                        financial information from Canalyst’s proprietary API
+                        financial information from Tegus’s proprietary API
                       </ListItem>
                       <ListItem>
                         Refactored original functionalities to accommodate
@@ -67,7 +69,37 @@ const ExperienceContainer = () => {
                       type: "easeInOut",
                     }}
                   >
-                    <Heading size="sm">Software Test Engineer Co-op</Heading>
+                    <Heading size="sm">Software Engineer Co-op</Heading>
+                    <Text fontSize={14}>May 2022 - August 2022</Text>
+                    <List styleType="disc" spacing={2} mt={5}>
+                      <ListItem>
+                        Developed and maintained C# and Node.js application for
+                        financial analysts to ensure consistency and accuracy
+                        across public equity models.
+                      </ListItem>
+                      <ListItem>
+                        Used HTML, CSS and Typescript to create an Excel Office
+                        Add-in with custom functions for clients to pull
+                        financial information from Canalyst’s proprietary API
+                      </ListItem>
+                      <ListItem>
+                        Refactored original functionalities to accommodate
+                        software design changes and added unit tests
+                      </ListItem>
+                      <Text as="i" fontSize={14}>* Canalyst was acquired by Tegus in August 2022</Text>
+                    </List>
+                  </motion.div>
+                </TabPanel>
+                <TabPanel>
+                  <motion.div
+                    initial={{ opacity: 0, x: 0, y: -20 }}
+                    animate={{ opacity: 1, x: 0, y: 0 }}
+                    transition={{
+                      duration: 0.5,
+                      type: "easeInOut",
+                    }}
+                  >
+                    <Heading size="sm">Software QA Developer Co-op</Heading>
                     <Text fontSize={14}>January 2022 - April 2022</Text>
                     <List styleType="disc" spacing={2} mt={5}>
                       <ListItem>
