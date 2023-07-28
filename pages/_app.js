77650a476dd7feee1c544a62350f0a08e153e54b
xianyao009 <yao009@hotmail.com>
2023-07-28 10:47:48 -0700

    Install vercel web analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Layout from "../components/container/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../components/Theme";
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
     <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </ChakraProvider>
   );
